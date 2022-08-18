22ba1b9778e534170d5934e76e615161dc1ce4e2
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-08-18 12:56:37 -0300

    create new todo

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,11 @@
+const NewTodo = () => {
+    return(
+        <form>
+            <label>Todo text</label>
+            <input type="text"/>
+            <button>Add todo</button>
+        </form>
+    )
+};
+
+export default NewTodo;
