99c8edd0f840f0ced26b36c9a8ff4c3919a997b9
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-08-18 12:50:16 -0300

    modify adding TodoItem component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,12 @@
-const Todos: React.FC<{items: string[]}> = (props) => {
+import Todo from '../models/todo';
+import TodoItem from './Todoitem';
+
+const Todos: React.FC<{items: Todo[]}> = (props) => {
     return (
         <ul>
-            {props.items.map((item) => (<li key={item}>{item}</li>))}
+            {props.items.map((item) => (
+                <TodoItem key={item.id} text={item.text}/>
+            ))}
         </ul>
     )
 };
