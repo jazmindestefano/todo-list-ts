e8d0a68248769911fa0242646029d42059147481
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-08-18 13:24:30 -0300

    create new todo form event

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,20 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = todoTextInputRef.current!.value;
+        if(enteredText.trim().length === 0){
+            //error
+            return;
+        }
+    };
+
     return(
-        <form>
-            <label>Todo text</label>
-            <input type="text"/>
+        <form onSubmit={submitHandler}>
+            <label htmlFor="text">Todo text</label>
+            <input type="text" id="text" ref={todoTextInputRef}/>
             <button>Add todo</button>
         </form>
     )
